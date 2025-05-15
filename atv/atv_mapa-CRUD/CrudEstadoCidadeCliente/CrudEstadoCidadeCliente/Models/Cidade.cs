f97c568d1d1927c4ef918352e45004eebd87ec41
gustavo-sosu <gustavo.porto1052@gmail.com>
2025-05-15 16:32:04 -0300

    comecando a atividade de estadoClienteCidade

---
+++ atv/atv_mapa-CRUD/CrudEstadoCidadeCliente/CrudEstadoCidadeCliente/Models/Cidade.cs
@@ -0,0 +1,13 @@
+﻿namespace CrudEstadoCidadeCliente.Models
+{
+    public class Cidade
+    {
+        public int CidadeId { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public decimal Preco { get; set; }
+
+        public int ClienteId { get; set; }
+        public Cliente? Cliente { get; set; }
+
+    }
+}
---
+++ atv/atv_mapa-CRUD/CrudEstadoCidadeCliente/CrudEstadoCidadeCliente/Models/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace CrudEstadoCidadeCliente.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        public ICollection<Produto>? Produtos { get; set; }
+
+    }
+}
